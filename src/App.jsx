a16c141bb17c36e2b6799122dea71af197d39f11
Molly Felts Vallin <mnfelts@gmail.com>
2023-02-21 22:02:01 +0100

    react-router setup & file org.
    - added react-router-dom to define routes
    - added react-router-hash-link to allow for linking to home page sections from the navbar
    - added comments to App.jsx and index.js files
    - created empty placeholder files for pages and components
    - moved .css files to /styles folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import logo from './assets/logo.svg';
-import './App.css';
+// use react router to control what is rendered in the virtual DOM
+import { Routes, Route } from "react-router-dom";
+
+// import CSS stylesheet
+import './styles/App.css';
+
+/** import website 'page' files and components
+ * HOME page : single-page scroll with Hero, Projects, and About sections
+ * Projects: card section that will open individual project page when card is clicked
+ * header and footer components (same throughout all site pages)
+ * ERROR page will be rendered if route or content is non-existent
+ */
+
+import Home from './pages/Home';
+import Project from './pages/Project';
+import Error from './pages/Error';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>⚙️ UNDER CONSTRUCTION ⚙️</p>
-      </header>
+      <Header className="header" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="projects/*" exact element={<Project />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer className="footer" />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
+/** use React
+ * createRoot connects React components to a DOM node (for rendering purposes)
+ * BrowserRouter allows for routing to different urls which informs React which 'page' to render
+ * reportWebVitals will log performance metrics in the console 
+ */
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// import stylesheet
+import './styles/index.css';
+
+// import App.jsx which defines routes to different 'pages'
+import App from './App';
+
+// render site (App.jsx) inside of the div with id of 'root' listed in index.html
+const root = createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
-reportWebVitals();
+reportWebVitals(console.log);
