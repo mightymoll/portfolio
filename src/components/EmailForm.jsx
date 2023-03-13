8bbfe643fbe8b192a98fe2501848b3344cde6890
Molly Felts Vallin <mnfelts@gmail.com>
2023-03-13 21:31:05 +0100

    contact  reconfig
    - created separate EmailForm.jsx component (imported to Contact page)
    - added state values to EmailForm to display different messages on error / success

---
+++ src/components/EmailForm.jsx
@@ -0,0 +1,67 @@
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
+function EmailForm() {
+  const form = useRef();
+  const [status, setStatus] = useState();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_an67sqn",
+      "template_bgbyr3d",
+      form.current,
+      "Hpk5w75e_cvizNC0o"
+    )
+      .then(() => {
+        setStatus('success');
+      })
+      .catch((error) => {
+        setStatus('error')
+        console.log("there was an error" + error);
+      });
+    e.target.reset();
+  }
+
+  if (status !== 'success') {
+    return (
+      <div>
+        <form className="contact_form" ref={form} onSubmit={sendEmail}>
+          <div className="sender">
+            <div className="form_group">
+              <label className="form_label">{'> Name :'}</label>
+              <input className="form_input" type="text" name="user_name" placeholder="Jacques Almanac" required />
+            </div>
+            <div className="form_group">
+              <label className="form_label">{'> Email :'}</label>
+              <input className="form_input" type="email" name="user_email" placeholder="emailhandle@sacrebleu.com" aria-describedby="emailHelp" required />
+            </div>
+          </div>
+          <div className="form_group">
+            <label className="form_label">{'> Message :'}</label>
+            <textarea className="form_message" name="message" placeholder="your message goes here; I know you know, but y'know..." required></textarea>
+          </div>
+          <button type="submit" className="button-submit">send via internet ✨MAGIC✨</button>
+        </form >
+        <div>{status === 'error' ?
+          <div className="form_error">
+            <p>SACRE BLEU 😱 that didn't seem to work, please email me directly :</p>
+            <a href="mailto:mnfelts@gmail.com">mnfelts@gmail.com</a>
+          </div>
+          : <div></div>}
+        </div>
+      </div >
+    )
+  }
+  else if (status === 'success') {
+    return (
+      <form className="contact_form success" ref={form} onSubmit={sendEmail}>
+        <p>Your message was sent successfully ! ✅ </p>
+      </form>
+    )
+  }
+}
+
+export default EmailForm
+
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,6 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import EmailForm from "../components/EmailForm"
 
 function Contact() {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm(
-      "service_an67sqn",
-      "template_bgbyr3d",
-      form.current,
-      "Hpk5w75e_cvizNC0o"
-    )
-      .then(
-        result => console.log(result.text),
-        error => console.log(error.text)
-      );
-    e.target.reset();
-  }
 
   return (
     <section id="contact">
@@ -29,21 +11,7 @@ function Contact() {
           <p>just want to say hi?</p>
           <p>found a bug?</p>
         </div>
-        <form className="contact-form box" ref={form} onSubmit={sendEmail}>
-          <div className="sender">
-            <div className="form_group">
-              <label className="form_label">{'> Name :'}</label>
-              <input className="form_input" type="text" name="user_name" placeholder="Jacques Almanac" required />
-            </div>
-            <div className="form_group">
-              <label className="form_label">{'> Email :'}</label>
-              <input className="form_input" type="email" name="user_email" placeholder="emailhandle@sacrebleu.com" aria-describedby="emailHelp" required />
-            </div>
-          </div>
-          <label className="form_label">{'> Message :'}</label>
-          <textarea className="form_message" name="message" placeholder="your message goes here; I know you know, but y'know..." required></textarea>
-          <button type="submit" className="button-submit">send via internet ✨MAGIC✨</button>
-        </form>
+        <EmailForm />
       </div>
     </section >
   );
