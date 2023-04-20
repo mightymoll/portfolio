b13934ddc57a0999eab3d87619c50e9a08e8f017
Molly Felts Vallin <mnfelts@gmail.com>
2023-04-20 22:06:55 +0200

    bug - footer (mobile)
    - .svg 'v' logo not showing correctly on mobile (issue with transform & translate)
    - updated logo to be .png file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { ReactComponent as VIcon } from '../assets/V.svg'
+import vIcon from '../assets/V.png'
 
 /** get current year to display in copyright text
  * must convert data to string to avoid object-type errors in React */
@@ -8,13 +8,17 @@ let year = currentYear.toString();
 function Footer() {
   return (
     <footer className='footer_container'>
-      <VIcon fill="#BDE0C0" transform='rotate(90),scale(-0.4, 0.4)' alt='>' />
-      {window.innerWidth < 360 ?
+      <div className='vIcon left'>
+        <img src={vIcon} alt='<' />
+      </div>
+      {window.innerWidth < 375 ?
         // remove word 'copyright' on small screens
         <p className='footer_copyright'>© {year} Molly Felts Vallin</p>
         :
         <p className='footer_copyright'>Copyright © {year} Molly Felts Vallin</p>}
-      <VIcon fill="#BDE0C0" transform='rotate(-90),translate(0,0) scale(-0.4, 0.4)' alt='>' />
+      <div className='vIcon right'>
+        <img src={vIcon} alt='>' />
+      </div>
     </footer >
   )
 };
