b8f0091f0a292f59e6495452644b7479b4938703
Molly Felts Vallin <mnfelts@gmail.com>
2023-04-20 20:40:37 +0200

    delete 'construction' files
    - removed all files related to placeholder for mobile

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { Routes, Route } from "react-router-dom";
 
 /** import website 'page' files and components
  * HOME page : single-page scroll with Hero, Projects, and About sections
- * Projects: card section that will open individual project page when card is clicked
  * header and footer components (same throughout all site pages)
  * ERROR page will be rendered if route or content is non-existent
  */
 
 import Home from './pages/Home';
-import ProjectPage from './pages/ProjectPage';
 import Error from './pages/Error';
 import Layout from "./pages/Layout";
 
@@ -19,7 +17,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />} >
           <Route index element={<Home />} />
-          <Route path="/projects/:id" element={<ProjectPage />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
